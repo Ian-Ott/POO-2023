2d53133d51955375e6176d741d3564754e7992ce
Ian-Ott <101023990+Ian-Ott@users.noreply.github.com>
2023-08-20 17:55:32 -0300

    agregadas implementaciones de lista (falta probar)

--- ar/edu/unlu/poo/lista/ListaEnlazada.java
+++ ar/edu/unlu/poo/lista/ListaEnlazada.java
@@ -20,7 +20,7 @@ public class ListaEnlazada {
 
     public String toString() {
         String acumulador = "";
-        Integer i = 1;
+        int i = 1;
         Nodo nodoAux = primero;
         if (primero == null) {
             acumulador = "lista vacia";
@@ -33,5 +33,60 @@ public class ListaEnlazada {
 
         }
         return acumulador;
+    }
+    public boolean es_vacia(){
+        return (primero == null);
+    }
+
+    public int longitud(){
+        int longitud = 0;
+        Nodo nodoAux = primero;
+        if (primero != null){
+            longitud++;
+        }
+        while(nodoAux != null){
+            longitud++;
+            nodoAux = nodoAux.getSiguiente();
+        }
+        return longitud;
+    }
+
+    public void eliminar(int pos){
+        int i = 1;
+        Nodo nodoAux = primero;
+        while(nodoAux != null){
+            if (pos == i){
+                nodoAux.setDato(nodoAux.getSiguiente());
+            }
+            i++;
+        }
+    }
+    public Object recuperar(int pos){
+        int i = 1;
+        Nodo nodoAux = primero;
+        while(nodoAux != null){
+            if (pos == i){
+                return nodoAux.getDato();
+            }
+            i++;
+            nodoAux = nodoAux.getSiguiente();
+        }
+        return "no se encontro la posicion";
+    }
+
+    public void insertar(Object dato, int pos){
+        Nodo nodoAux = primero;
+        Nodo nuevo_nodo = new Nodo();
+        nuevo_nodo.setDato(dato);
+        if (pos == 1){
+            nuevo_nodo.setSiguiente(primero);
+            primero = nuevo_nodo;
+        }else{
+        for (int i = 0; i < pos - 2; i++){
+            nodoAux = nodoAux.getSiguiente();
+        }
+        nuevo_nodo.setSiguiente(nodoAux.getSiguiente());
+        nodoAux.setSiguiente(nuevo_nodo);
+        }
     }
 }
